fcad5ceba7e68ae616184ecb14bbc115eb01d2f2
Luigi Pinca <luigipinca@gmail.com>
2019-06-26 16:02:08 +0200

    Update constraints for the SPICE-BCH pair

--- index.js
+++ index.js
@@ -18,12 +18,27 @@ const limits = {
       buy: { min: Big(0.0001), max: Big(100000) },
       sell: { min: Big(0.00001), max: Big(10000) }
     }
+  },
+  'SPICE-BCH': {
+    limit: {
+      buy: {
+        quantity: { min: Big(1), max: Big(1000) },
+        price: { min: Big(0.00000001), max: Big(1000) }
+      },
+      sell: {
+        quantity: { min: Big(1), max: Big(1000) },
+        price: { min: Big(0.00000001), max: Big(1000) }
+      }
+    },
+    market: {
+      buy: { min: Big(0.000001), max: Big(100000) },
+      sell: { min: Big(1), max: Big(1000000) }
+    }
   }
 };
 
 limits['BSV-BCH'] = limits['BTC-BCH'];
 limits['ETH-BCH'] = limits['BTC-BCH'];
 limits['LTC-BCH'] = limits['BTC-BCH'];
-limits['SPICE-BCH'] = limits['BTC-BCH'];
 
 module.exports = limits;
