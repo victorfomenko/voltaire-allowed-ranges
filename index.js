ab18a9c8221e3b767d4c4436ba1283328f62be34
Luigi Pinca <luigipinca@gmail.com>
2019-08-26 16:39:35 +0200

    Add constraints for the BCH-USDH, DROP-BCH, and SAI-BCH pairs

--- index.js
+++ index.js
@@ -34,11 +34,44 @@ const limits = {
       buy: { min: Big(0.000001), max: Big(100) },
       sell: { min: Big(1), max: Big(1000000) }
     }
+  },
+  'SAI-BCH': {
+    limit: {
+      buy: {
+        quantity: { min: Big(0.1), max: Big(1000000) },
+        price: { min: Big(0.0000001), max: Big(10) }
+      },
+      sell: {
+        quantity: { min: Big(0.1), max: Big(1000000) },
+        price: { min: Big(0.00000001), max: Big(10) }
+      }
+    },
+    market: {
+      buy: { min: Big(0.000001), max: Big(100) },
+      sell: { min: Big(0.1), max: Big(1000000) }
+    }
+  },
+  'BCH-USDH': {
+    limit: {
+      buy: {
+        quantity: { min: Big(0.01), max: Big(1000) },
+        price: { min: Big(1), max: Big(100000) }
+      },
+      sell: {
+        quantity: { min: Big(0.00001), max: Big(1000) },
+        price: { min: Big(0.01), max: Big(100000) }
+      }
+    },
+    market: {
+      buy: { min: Big(0.01), max: Big(100000) },
+      sell: { min: Big(0.00001), max: Big(10000) }
+    }
   }
 };
 
 limits['BSV-BCH'] = limits['BTC-BCH'];
 limits['ETH-BCH'] = limits['BTC-BCH'];
 limits['LTC-BCH'] = limits['BTC-BCH'];
+limits['DROP-BCH'] = limits['SAI-BCH'];
 
 module.exports = limits;
